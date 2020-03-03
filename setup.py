392ad2bf831f75ed779280213062af9ef2286046
Derryn Knife <derryn.knife@gmail.com>
2020-03-03 21:21:22 +1000

    not sure why im committing a setup file...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reliafy_surpyval", # Replace with your own username
-    version="0.4",
+    version="0.4.1",
     author="Derryn Knife",
     author_email="derryn@reliafy.com",
     description="A survival analysis python package",
