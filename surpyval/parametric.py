d2d28bc960b84d481929634a0db88476723ffa5d
Derryn Knife <derryn.knife@gmail.com>
2020-03-16 09:27:06 +1000

    Added logistic distribution

--- surpyval/parametric.py
+++ surpyval/parametric.py
@@ -1314,6 +1314,66 @@ class Weibull_Mix_Two_(SurpyvalDist):
 		model.params = tuple((self.n, wmm.w, wmm.alphas, wmm.betas))
 
 		return model
+class Logistic_(SurpyvalDist):
+	def __init__(self, name):
+		self.name = name
+		self.k = 2
+		self.bounds = ((None, None), (0, None),)
+		self.use_autograd = True
+		self.plot_x_scale = 'linear'
+		self.y_ticks = [0.0001, 0.0002, 0.0003, 0.001, 0.002, 
+			0.003, 0.005, 0.01, 0.02, 0.03, 0.05, 
+			0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 
+			0.9, 0.95, 0.99, 0.999, 0.9999]
+
+	def parameter_initialiser(self, x, c=None, n=None):
+		flag = (c == 0).astype(np.int)
+		return x.sum() / (n * flag).sum(), 1.
+
+	def sf(self, x, mu, sigma):
+		return 1 - self.ff(x, mu, sigma)
+
+	def ff(self, x, mu, sigma):
+		z = (x - mu) / sigma
+		return 1. / (1 + np.exp(-z))
+
+	def df(self, x, mu, sigma):
+		z = (x - mu) / sigma
+		return np.exp(-z) / (sigma * (1 + np.exp(-z))**2)
+
+	def hf(self, x, mu, sigma):
+		return self.pdf(x, mu, sigma) / self.sf(x, mu, sigma)
+
+	def Hf(self, x, mu, sigma):
+		return -np.log(self.sf(x, mu, sigma))
+
+	def qf(self, p, mu, sigma):
+		return mu + sigma * np.log(p/(1 - p))
+
+	def mean(self, mu, sigma):
+		return mu
+
+	def random(self, size, mu, sigma):
+		U = uniform.rvs(size=size)
+		return self.qf(U, mu, sigma)
+
+	def mpp_x_transform(self, x):
+		return x
+
+	def mpp_y_transform(self, y):
+		return -np.log(1./y - 1)
+
+	def mpp_inv_y_transform(self, y):
+		return 1./(np.exp(-y) + 1)
+
+	def unpack_rr(self, params, rr):
+		if   rr == 'y':
+			sigma = 1/params[0]
+			mu    = -sigma * params[1]
+		elif rr == 'x':
+			sigma  = 1./params[0]
+			mu = np.exp(params[1] / (beta * params[0]))
+		return mu, sigma
 
 class WMM(): 
 	def __init__(self, **kwargs): 
@@ -1423,4 +1483,5 @@ Normal = Normal_('Normal')
 LogNormal = LogNormal_('LogNormal')
 Uniform = Uniform_('Uniform')
 Weibull_Mix_Two = Weibull_Mix_Two_('Weibull_Mix_Two')
+Logistic = Logistic_('Logistic')
 
