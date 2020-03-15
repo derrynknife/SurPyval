13aaf513d1196a653e7a5d0ff3c4a5c7ee42b9cd
Derryn Knife <derryn.knife@gmail.com>
2020-03-16 09:46:28 +1000

    Added loglogistic. Now how all dists in survreg R package

--- surpyval/parametric.py
+++ surpyval/parametric.py
@@ -1374,6 +1374,67 @@ class Logistic_(SurpyvalDist):
 			sigma  = 1./params[0]
 			mu = np.exp(params[1] / (beta * params[0]))
 		return mu, sigma
+class LogLogistic_(SurpyvalDist):
+	def __init__(self, name):
+		self.name = name
+		self.k = 2
+		self.bounds = ((None, None), (0, None),)
+		self.use_autograd = True
+		self.plot_x_scale = 'log'
+		self.y_ticks = [0.0001, 0.0002, 0.0003, 0.001, 0.002, 
+			0.003, 0.005, 0.01, 0.02, 0.03, 0.05, 
+			0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 
+			0.9, 0.95, 0.99, 0.999, 0.9999]
+
+	def parameter_initialiser(self, x, c=None, n=None):
+		flag = (c == 0).astype(np.int)
+		return x.sum() / (n * flag).sum(), 2.
+
+	def sf(self, x, alpha, beta):
+		return 1 - self.ff(x, alpha, beta)
+
+	def ff(self, x, alpha, beta):
+		return 1. / (1 + (x/alpha)**-beta)
+
+	def df(self, x, alpha, beta):
+		return ((beta/alpha)*(x/alpha)**(beta-1.))/((1. + (x/alpha)**beta)**2.)
+
+	def hf(self, x, alpha, beta):
+		return self.pdf(x, alpha, beta) / self.sf(x, alpha, beta)
+
+	def Hf(self, x, alpha, beta):
+		return -np.log(self.sf(x, alpha, beta))
+
+	def qf(self, p, alpha, beta):
+		return alpha * (p/(1 - p))**(1./beta)
+
+	def mean(self, alpha, beta):
+		if beta > 1:
+			return (alpha * np.pi / beta) / (np.sin(np.pi/beta))
+		else:
+			return np.nan
+
+	def random(self, size, alpha, beta):
+		U = uniform.rvs(size=size)
+		return self.qf(U, alpha, beta)
+
+	def mpp_x_transform(self, x):
+		return np.log(x)
+
+	def mpp_y_transform(self, y):
+		return -np.log(1./y - 1)
+
+	def mpp_inv_y_transform(self, y):
+		return 1./(np.exp(-y) + 1)
+
+	def unpack_rr(self, params, rr):
+		if rr == 'y':
+			beta  = params[0]
+			alpha = np.exp(params[1]/-beta)
+		elif rr == 'x':
+			beta  = 1./params[0]
+			alpha = np.exp(params[1] / (beta * params[0]))
+		return alpha, beta
 
 class WMM(): 
 	def __init__(self, **kwargs): 
@@ -1472,8 +1533,6 @@ class WMM():
 	def sf(self, t): 
 		return 1 - self.ff
 
-
-
 Weibull = Weibull_('Weibull')
 Gumbel = Gumbel_('Gumbel')
 Exponential = Exponential_('Exponential')
@@ -1484,4 +1543,5 @@ LogNormal = LogNormal_('LogNormal')
 Uniform = Uniform_('Uniform')
 Weibull_Mix_Two = Weibull_Mix_Two_('Weibull_Mix_Two')
 Logistic = Logistic_('Logistic')
+LogLogistic = LogLogistic_('LogLogistic')
 
