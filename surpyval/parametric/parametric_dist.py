fded56e811546ad276ec093aba63274bbfdf3b23
Derryn Knife <derryn.knife@gmail.com>
2020-03-16 14:16:38 +1000

    Separated out Parametric and Weibull. Nothing seems broken yet

---
+++ surpyval/parametric/parametric_dist.py
@@ -0,0 +1,223 @@
+import re
+from autograd import jacobian
+import autograd.numpy as np
+from scipy.stats import uniform
+
+from scipy.special import ndtri as z
+from surpyval import nonparametric as nonp
+from surpyval import parametric as para
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator
+
+NUM     = np.float64
+TINIEST = np.finfo(NUM).tiny
+EPS     = np.sqrt(np.finfo(NUM).eps)
+
+from surpyval.parametric.surpyval_dist import SurpyvalDist
+
+class Parametric():
+	def __init__(self):
+		self.jac_r = np.vectorize(jacobian(lambda x : self.dist.sf(x, *self.params)))
+
+	def sf(self, x):
+		return self.dist.sf(x, *self.params)
+
+	def ff(self, x):
+		return self.dist.ff(x, *self.params)
+
+	def df(self, x): 
+		return self.dist.df(x, *self.params)
+
+	def hf(self, x):
+		return self.dist.hf(x, *self.params)
+
+	def Hf(self, x):
+		return self.dist.Hf(x, *self.params)
+
+	def qf(self, p):
+		return self.dist.qf(p, *self.params)
+
+	def random(self, size):
+		U = uniform.rvs(size=size)
+		return self.qf(U, *self.params)
+
+	def mean(self):
+		return self.dist.mean(*self.params)
+
+	def moment(self):
+		return self.dist.moment(*self.params)
+
+	def entropy(self):
+		return self.dist.entropy(*self.params)
+
+	def cb(self, x, sig):
+		if self.method != 'MLE':
+			raise Exception('Only MLE has confidence bounds')
+			
+		du = z(sig) * np.sqrt(var_u)
+		u_u = u_hat + du
+		u_l = u_hat - du
+		return np.vstack([np.exp(-np.exp(u_u)), np.exp(-np.exp(u_l))])
+
+	def ll(self):
+		if hasattr(self, 'log_like'):
+			return self.log_like
+		else:
+			x = self.data['x']
+			c = self.data['c']
+			n = self.data['n']
+			self.log_like = -self.dist.neg_ll(x, c, n, *self.params)
+			return self.log_like
+
+	def aic(self):
+		if hasattr(self, 'aic_'):
+			return self.aic_
+		else:
+			x = self.data['x']
+			c = self.data['c']
+			n = self.data['n']
+			alpha = self.alpha
+			beta  = self.beta
+			k = len(self.params)
+			self.aic_ = 2 * k + 2 * self.dist.neg_ll(x, c, n, *self.params)
+			return self.aic_
+
+	def aic_c(self):
+		if hasattr(self, 'aic_c_'):
+			return self.aic_c_
+		else:
+			k = len(self.params)
+			n = len(self.data['x'])
+			self.aic_c_ = self.aic() + (2*k**2 + 2*k)/(n - k - 1)
+			return self.aic_c_
+
+	def get_plot_data(self, heuristic, plot_bounds=True, cb=0.05):
+		"""
+		Looking a little less ugly now.
+		"""
+		x = self.data['x']
+		x_, r, d, F = nonp.plotting_positions(
+			self.data['x'], 
+			c=self.data['c'], 
+			n=self.data['n'], 
+			heuristic=heuristic)
+
+		if self.dist.name in ['Weibull3p']:
+			x_ = x_ - self.params[2]
+			x = x - self.params[2]
+		y_scale_min = np.min(F[F > 0])/2
+		y_scale_max = (1 - (1 - np.max(F[F < 1]))/10)
+
+		# x-axis
+		x_min = np.min(x)
+		x_max = np.max(x)
+		if self.dist.plot_x_scale == 'log':
+			x_min = np.log10(x_min)
+			x_max = np.log10(x_max)
+			vals_non_sig = 10 ** np.linspace(x_min, x_max, 7)
+			x_minor_ticks = np.arange(np.floor(x_min), np.ceil(x_max))
+			x_minor_ticks = ((10**x_minor_ticks * np.array(np.arange(1, 11))
+												    .reshape((10, 1)))
+												    .flatten())
+			diff = (x_max - x_min)/10
+			x_scale_min = 10**(x_min - diff)
+			x_scale_max = 10**(x_max + diff)
+			x_model = 10**np.linspace(x_min - diff, x_max + diff, 100)
+		else:
+			vals_non_sig = np.linspace(x_min, x_max, 7)
+			x_minor_ticks = np.arange(np.floor(x_min), np.ceil(x_max))
+			diff = (x_max - x_min) / 10
+			x_scale_min = x_min - diff
+			x_scale_max = x_max + diff
+			x_model = np.linspace(x_scale_min, x_scale_max, 100)
+
+		if self.dist.name in ['Weibull3p']:
+			cdf = self.ff(x_model + self.params[2])
+		else:
+			cdf = self.ff(x_model)
+		not_different = True
+		i = 1
+		while not_different:
+		    x_ticks = np.array(para.round_sig(vals_non_sig, i))
+		    not_different = (np.diff(x_ticks) == 0).any()
+		    i += 1
+
+		y_ticks = np.array(self.dist.y_ticks)
+		y_ticks = y_ticks[np.where((y_ticks > y_scale_min) & 
+								   (y_ticks < y_scale_max))[0]]
+		y_ticks_labels = [str(int(y))+'%' if (re.match('([0-9]+\.0+)', str(y)) is not None) & 
+							(y > 1) else str(y) for y in y_ticks * 100]
+
+		x_ticks_labels = [str(int(x)) if (re.match('([0-9]+\.0+)', str(x)) is not None) & 
+							(x > 1) else str(x) for x in x_ticks]
+
+		if plot_bounds:
+			if self.dist.name == 'Weibull3p':
+				cbs = 1 - self.dist.R_cb(x_model + self.params[2], *self.params, self.hess_inv, cb=cb)
+			else:
+				cbs = 1 - self.dist.R_cb(x_model, *self.params, self.hess_inv, cb=cb)
+		else:
+			cbs = []
+
+		plot_data = {
+			'x_scale_min' : x_scale_min,
+			'x_scale_max' : x_scale_max,
+			'y_scale_min' : y_scale_min,
+			'y_scale_max' : y_scale_max,
+			'y_ticks' : y_ticks,
+			'y_ticks_labels' : y_ticks_labels,
+			'x_ticks' : x_ticks,
+			'x_ticks_labels' : x_ticks_labels,
+			'cdf' : cdf,
+			'x_model' : x_model,
+			'x_minor_ticks' : x_minor_ticks,
+			'cbs' : cbs,
+			'x_scale' : self.dist.plot_x_scale,
+			'x_' : x_,
+			'F' : F
+		}
+		return plot_data
+
+	def plot(self, heuristic, plot_bounds=True, cb=0.05):
+		d = self.get_plot_data(heuristic, plot_bounds=plot_bounds, cb=cb)
+		# MAKE THE PLOT
+		# Set the y limits
+		plt.gca().set_ylim([d['y_scale_min'], d['y_scale_max']])
+		
+		# Set the x scale
+		plt.xscale(d['x_scale'])
+		# Set the y scale
+		if self.dist.name == 'Gamma':
+			# The y scale for the gamm distribution is dependent on the shape
+			plt.gca().set_yscale('function',
+				functions=(lambda x : self.dist.mpp_y_transform(x, self.params[0]),
+						   lambda x : self.dist.mpp_inv_y_transform(x, self.params[0])))
+		else:
+			plt.gca().set_yscale('function', 
+				functions=(self.dist.mpp_y_transform, 
+						   self.dist.mpp_inv_y_transform))
+		
+		# Set Major Y axis ticks
+		plt.yticks(d['y_ticks'], labels=d['y_ticks_labels'])
+		# Set Minor Y axis ticks
+		plt.gca().yaxis.set_minor_locator(FixedLocator(np.linspace(0, 1, 51)))
+		# Set Major X axis ticks
+		plt.xticks(d['x_ticks'], labels=d['x_ticks_labels'])
+		# Set Minor X axis ticks if log scale.
+		if d['x_scale'] == 'log':
+			plt.gca().set_xticks(d['x_minor_ticks'], minor=True)
+			plt.gca().set_xticklabels([], minor=True)
+
+		# Turn on the grid
+		plt.grid(b=True, which='major', color='g', alpha=0.4, linestyle='-')
+		plt.grid(b=True, which='minor', color='g', alpha=0.1, linestyle='-')
+
+		# Label it
+		plt.title('{} Probability Plot'.format(self.dist.name))
+		plt.ylabel('CDF')
+		plt.scatter(d['x_'], d['F'])
+		plt.gca().set_xlim([d['x_scale_min'], d['x_scale_max']])
+		if plot_bounds:
+			plt.plot(d['x_model'], d['cbs'], color='r')
+		return plt.plot(d['x_model'], d['cdf'], color='k', linestyle='--')
---
+++ surpyval/parametric/weibull_dist.py
@@ -0,0 +1,148 @@
+import re
+from autograd import jacobian, hessian
+import autograd.numpy as np
+from autograd.numpy.linalg import inv
+from autograd.scipy.stats import norm
+from scipy.stats import norm as scipy_norm
+from scipy.stats import uniform
+from numpy import euler_gamma
+
+from scipy.special import gamma as gamma_func
+from scipy.special import gammainc, gammaincinv
+from scipy.special import factorial
+from scipy.optimize import minimize
+from scipy.special import ndtri as z
+from scipy.optimize import approx_fprime
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator
+
+from surpyval import nonparametric as nonp
+from surpyval import parametric as para
+from surpyval.parametric.surpyval_dist import SurpyvalDist
+from surpyval.parametric.parametric_dist import Parametric
+
+class Weibull_(SurpyvalDist):
+	def __init__(self, name):
+		self.name = name
+		# Set 'k', the number of parameters
+		self.k = 2
+		self.bounds = ((0, None), (0, None),)
+		self.use_autograd = True
+		self.plot_x_scale = 'log'
+		self.y_ticks = [0.0001, 0.0002, 0.0003, 0.001, 0.002, 
+			0.003, 0.005, 0.01, 0.02, 0.03, 0.05, 
+			0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 
+			0.9, 0.95, 0.99, 0.999, 0.9999]
+
+	def parameter_initialiser(self, x, c=None, n=None):
+		gumb = para.Gumbel.fit(np.log(x), c, n, how='MLE')
+		if not gumb.res.success:
+			gumb = para.Gumbel.fit(np.log(x), c, n, how='MPP')
+		mu, sigma = gumb.params
+		alpha, beta = np.exp(mu), 1. / sigma
+		return np.exp(mu), 1 / sigma
+
+	def sf(self, x, alpha, beta):
+		return np.exp(-(x / alpha)**beta)
+
+	def ff(self, x, alpha, beta):
+		return 1 - np.exp(-(x / alpha)**beta)
+
+	def df(self, x, alpha, beta):
+		return (beta / alpha) * (x / alpha)**(beta-1) * np.exp(-(x / alpha)**beta)
+
+	def hf(self, x, alpha, beta):
+		return (beta / alpha) * (x / alpha)**(beta - 1)
+
+	def Hf(self, x, alpha, beta):
+		return (x / alpha)**beta
+
+	def qf(self, p, alpha, beta):
+		return alpha * (-np.log(1 - p))**(1/beta)
+
+	def mean(self, alpha, beta):
+		return alpha * gamma_func(1 + 1./beta)
+
+	def moment(self, n, alpha, beta):
+		return alpha**n * gamma_func(1 + n/beta)
+
+	def entropy(self, alhpa, beta):
+		return euler_gamma * (1 - 1/beta) + np.log(alpha / beta) + 1
+
+	def random(self, size, alpha, beta):
+		U = uniform.rvs(size=size)
+		return self.qf(U, alpha, beta)
+
+	def mpp_x_transform(self, x):
+		return np.log(x)
+
+	def mpp_y_transform(self, y):
+		return np.log(-np.log((1 - y)))
+
+	def mpp_inv_y_transform(self, y):
+		return 1 - np.exp(-np.exp(y))
+
+	def unpack_rr(self, params, rr):
+		if rr == 'y':
+			beta  = params[0]
+			alpha = np.exp(params[1]/-beta)
+		elif rr == 'x':
+			beta  = 1./params[0]
+			alpha = np.exp(params[1] / (beta * params[0]))
+		return alpha, beta
+
+	def u(self, x, alpha, beta):
+		return beta * (np.log(x) - np.log(alpha))
+
+	def u_cb(self, x, alpha, beta, cv_matrix, cb=0.05):
+		u = self.u(x, alpha, beta)
+		var_u = self.var_u(x, alpha, beta, cv_matrix)
+		diff = z(cb/2) * np.sqrt(var_u)
+		bounds = u + np.array([1., -1.]).reshape(2, 1) * diff
+		return bounds
+
+	def du(self, x, alpha, beta):
+		du_dbeta = np.log(x) - np.log(alpha)
+		du_dalpha  = -beta/alpha
+		return du_dalpha, du_dbeta
+
+	def var_u(self, x, alpha, beta, cv_matrix):
+		da, db = self.du(x, alpha, beta)
+		var_u = (da**2 * cv_matrix[0, 0] + db**2 * cv_matrix[1, 1] + 
+			2 * da * db * cv_matrix[0, 1])
+		return var_u
+
+	def R_cb(self, x, alpha, beta, cv_matrix, cb=0.05):
+		return np.exp(-np.exp(self.u_cb(x, alpha, beta, cv_matrix, cb))).T
+
+	def jacobian(self, x, alpha, beta, c=None, n=None):
+		"""
+		The jacobian for a two parameter Weibull distribution.
+
+		Please report mistakes if found!
+		"""
+		f = c == 0
+		l = c == -1
+		r = c == 1
+		dll_dbeta = (
+			1./beta * np.sum(n[f]) +
+			np.sum(n[f] * np.log(x[f]/alpha)) - 
+			np.sum(n[f] * (x[f]/alpha)**beta * np.log(x[f]/alpha)) - 
+			np.sum(n[r] * (x[r]/alpha)**beta * np.log(x[r]/alpha)) +
+			np.sum(n[l] * (x[l]/alpha)**beta * np.log(x[l]/alpha) *
+				np.exp(-(x[l]/alpha)**beta) / 
+				(1 - np.exp(-(x[l]/alpha)**beta)))
+		)
+
+		dll_dalpha = ( 0 -
+			beta/alpha * np.sum(n[f]) +
+			beta/alpha * np.sum(n[f] * (x[f]/alpha)**beta) +
+			beta/alpha * np.sum(n[r] * (x[r]/alpha)**beta) -
+			beta/alpha * np.sum(n[l] * (x[l]/alpha)**beta * 
+				np.exp(-(x[l]/alpha)**beta) /
+				(1 - np.exp(-(x[l]/alpha)**beta)))
+		)
+		return -np.array([dll_dalpha, dll_dbeta])
+
+Weibull = Weibull_('Weibull')
