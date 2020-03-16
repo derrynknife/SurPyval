3c2ce78aacfbffa5d3bcf2d3efcdeea47083c000
Derryn Knife <derryn.knife@gmail.com>
2020-03-16 12:18:11 +1000

    Cleaned up import statements

--- surpyval/__init__.py
+++ surpyval/__init__.py
@@ -1,4 +1,19 @@
 import surpyval.datasets
 
-from .nonparametric import *
-from .parametric import *
+from surpyval.parametric import Weibull
+from surpyval.parametric import Gumbel
+from surpyval.parametric import Exponential
+from surpyval.parametric import Gamma
+from surpyval.parametric import Weibull3p
+from surpyval.parametric import Normal
+from surpyval.parametric import LogNormal
+from surpyval.parametric import Uniform
+from surpyval.parametric import Weibull_Mix_Two
+from surpyval.parametric import Logistic
+from surpyval.parametric import LogLogistic
+from surpyval.parametric import WMM
+
+from surpyval.nonparametric import plotting_positions, filliben, nelson_aalen
+from surpyval.nonparametric import fleming_harrington, kaplan_meier, success_run
+from surpyval.nonparametric import get_x_r_d, xrd_to_tcn, rank_adjust, turnbull
+from surpyval.nonparametric import NonParametric
--- surpyval/nonparametric.py
+++ surpyval/nonparametric.py
@@ -4,6 +4,7 @@ import pandas as pd
 from scipy.stats import t, norm
 from scipy.stats import rankdata
 from scipy.special import ndtri as z
+from itertools import tee
 
 NUM = np.float64
 
@@ -34,6 +35,72 @@ event happened on the timeline.
 - Censoring vectors. Pass repeat x's not censoring counts.
 - Count doesn't assume unique x, it assumes repeats of censoring possible
 """
+
+def pairwise(iterable):
+    """
+    s -> (s0, s1), (s1, s2), (s2, s3), ...
+    """
+    a, b = tee(iterable, 2)
+    next(b, None)
+    return zip(a, b)
+
+def find_turnbull_bounds(left, right):
+    left = np.unique(left[left > 0])
+    right = np.unique(right)
+    left.sort()
+    right.sort()
+    flag = np.hstack([np.zeros_like(left), np.ones_like(right)]).astype(int)
+    tau = np.hstack([left, right])
+    idx = np.argsort(flag, kind='stable')[::-1]
+    flag = flag[idx]
+    tau = tau[idx]
+    idx = np.argsort(tau, kind='stable')
+    flag = flag[idx]
+    tau = tau[idx]
+    bounds = []
+    for i, (l, r) in enumerate(pairwise(flag)):
+        if ((l == 0) and (r == 1)):
+            bounds.append((tau[i], tau[i+1]))
+    return bounds
+
+def turnbull(left, right):
+    max_iter = 1000
+    intervals = find_turnbull_bounds(left, right)
+    
+    m = len(intervals)
+    n = len(left)
+    p = np.ones(m)/m
+    alphas = np.zeros((n, m))
+    for j, (l, r) in enumerate(intervals):
+        alphas[:, j] = (((left  < r) & (left  > l)) |
+                        ((right < r) & (right > l)) |
+                        ((right > r) & (left  < r)) |
+                        ((left  < l) & (right > l)) |
+                        #((l == left)) |
+                        ((r == right))).astype(int)
+    d = np.zeros(m)
+    iters = 0
+    p_1 = np.zeros_like(p)
+    while (not np.isclose(p, p_1).all()) and (iters < max_iter):
+        p_1 = p
+        iters += 1
+        conditional_p = np.zeros_like(alphas)
+        denom = np.zeros(n)
+        for i in range(n):
+            denom[i] = (alphas[i, :] * p).sum()
+        for j in range(m):
+            conditional_p[:, j] = alphas[:, j] * p[j]
+        d = (conditional_p / np.atleast_2d(denom).T).sum(axis=0)
+        #p = d/n
+        
+        r = np.ones_like(d) * n + d
+        r = r - d.cumsum()
+        R = np.cumprod(1 - d/r)
+        p = np.abs(np.diff(np.hstack([[1], R])))
+    
+    #r = np.ones_like(d) * n + d
+    #r = r - d.cumsum()
+    return intervals, p, d, r
 
 def plotting_positions(x, c=None, n=None, heuristic="Blom", A=None, B=None):
     # Need some error catching on the A and B thing
