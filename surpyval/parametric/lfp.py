286312bb330a29075c2d6219de868d5993eeb0ec
Derryn Knife <derryn.knife@gmail.com>
2020-03-18 19:59:45 +1000

    LFP done, caution should be given when using large data sets.

--- surpyval/parametric/lfp.py
+++ surpyval/parametric/lfp.py
@@ -1,4 +1,5 @@
 import autograd.numpy as np
+from autograd import jacobian, hessian
 from scipy.stats import uniform
 from numpy import euler_gamma
 from scipy.special import gamma as gamma_func
@@ -12,19 +13,18 @@ from surpyval.parametric.surpyval_dist import SurpyvalDist
 
 TINIEST = np.finfo(np.float64).tiny
 
-class LFP():
-	def __init__(self, name, dist):
-		self.name = name
+class LFP(SurpyvalDist):
+	def __init__(self, dist):
 		self.dist = dist
+		self.bounds = tuple(((0, 1), *self.dist.bounds))
+		self.use_autograd = True
 
-	def neg_ll(self, x, c, n, *params):
+	def neg_ll__(self, x, c, n, *params):
 		params = np.array(params)
-		w = params[0]
-		params = params[1::]
 		like = np.zeros_like(x)
-		like = np.where(c ==  0, self.df(x, w, *params), like)
-		like = np.where(c == -1, self.ff(x, w, *params), like)
-		like = np.where(c ==  1, self.sf(x, w, *params), like)
+		like = np.where(c ==  0, self.df(x, *params), like)
+		like = np.where(c == -1, self.ff(x, *params), like)
+		like = np.where(c ==  1, self.sf(x, *params), like)
 		like += TINIEST
 		like = np.where(like < 1, like, 1)
 		like = np.log(like)
@@ -32,7 +32,7 @@ class LFP():
 		return -like.sum()
 
 	def parameter_initialiser(self, x, c=None, n=None):
-		return self.dist.parameter_initialiser(x, c, n)
+		return tuple((1, *self.dist.parameter_initialiser(x, c, n)))
 
 	def sf(self, x, w, *params):
 		return 1 - self.ff(x, w, *params)
@@ -49,13 +49,16 @@ class LFP():
 	def Hf(self, x, w, *params):
 		return -np.log(self.sf(x, w, *params))
 
-	def _mle(self, x, c, n):
-		fun = lambda params : self.neg_ll(x, c, n, *params)
-		bounds = tuple(((0, 1), *self.dist.bounds))
+	def _mle__(self, x, c, n):
+		fun = lambda t : self.neg_ll(x, c, n, *t)
+		jac = jacobian(fun)
+		hess = hessian(fun)
 		init = (1., *self.parameter_initialiser(x, c, n))
-		res = minimize(fun, init, bounds=bounds, tol=1e-10)
+		res = minimize(fun, init, method='trust-exact', 
+			jac=jac, hess=hess, tol=1e-10, bounds=self.bounds)
+		#res = minimize(fun, init, bounds=bounds, tol=1e-20)
 		self.res = res
 
-	def fit(self, x, c=None, n=None):
+	def fit__(self, x, c=None, n=None):
 		x, c, n = surpyval.xcn_handler(x, c, n)
 		self._mle(x, c, n)
