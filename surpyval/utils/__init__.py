e1fd821e7ac2768258e118342336e5eda04a0a8e
Derryn Knife <derryn.knife@gmail.com>
2020-11-10 18:37:09 +1000

    Improved logic in xcn handler

--- surpyval/utils/__init__.py
+++ surpyval/utils/__init__.py
@@ -6,36 +6,36 @@ def group_xcn(x, c, n):
 	Ensures that all unique combinations of x and x are grouped. This should 
 	reduce the chance of having arrays that are too long for the MLE fit.
 	"""
-    grouped = defaultdict(lambda: defaultdict(int))
-    if x.ndim == 2:
-        for vx, vc, vn in zip(x, c, n):
-            grouped[tuple(vx)][vc] += vn
-    else:
-        for vx, vc, vn in zip(x, c, n):
-            grouped[vx][vc] += vn
+	grouped = defaultdict(lambda: defaultdict(int))
+	if x.ndim == 2:
+		for vx, vc, vn in zip(x, c, n):
+			grouped[tuple(vx)][vc] += vn
+	else:
+		for vx, vc, vn in zip(x, c, n):
+			grouped[vx][vc] += vn
 
-    x_out = []
-    c_out = []
-    n_out = []
+	x_out = []
+	c_out = []
+	n_out = []
 
-    for k, v in grouped.items():
-        for cv, nv in v.items():
-            x_out.append(k)
-            c_out.append(cv)
-            n_out.append(nv)
-    return np.array(x_out), np.array(c_out), np.array(n_out)
+	for k, v in grouped.items():
+		for cv, nv in v.items():
+			x_out.append(k)
+			c_out.append(cv)
+			n_out.append(nv)
+	return np.array(x_out), np.array(c_out), np.array(n_out)
 
-def x_tolist(x):
+def surv_tolist(x):
 	"""
 	supplement to the .tolist() function where there is mixed scalars and arrays.
 
 	Survival data can be confusing sometimes. I'm probably not helping by trying
 	to make it fit with one input.
 	"""
-    if x.ndim == 2:        
-        return [v[0] if v[0] == v[1] else v.tolist() for v in x]
-    else:
-        return x.tolist()
+	if x.ndim == 2:        
+		return [v[0] if v[0] == v[1] else v.tolist() for v in x]
+	else:
+		return x.tolist()
 
 def xcn_sort(x, c, n):
 	"""
@@ -99,63 +99,61 @@ def xcn_handler(x, c=None, n=None):
 		array of count of observations at to output array x and with censoring c. If n was None, count array assumed to be all one observation.
 	"""
 	if type(x) == list:
-        if any([list == type(v) for v in x]):
-            x_ndarray = np.empty(shape=(len(x), 2))
-            for idx, val in enumerate(x):
-                x_ndarray[idx, :] = np.array(val)
-            x = x_ndarray
-        else:
-            x = np.array(x).astype(np.float)
-            
-    assert x.ndim < 3, "variable array must be one or two dimensional"
+		if any([list == type(v) for v in x]):
+			x_ndarray = np.empty(shape=(len(x), 2))
+			for idx, val in enumerate(x):
+				x_ndarray[idx, :] = np.array(val)
+			x = x_ndarray
+		else:
+			x = np.array(x).astype(np.float)
+			
+	assert x.ndim < 3, "variable array must be one or two dimensional"
 
-    if x.ndim == 2:
-        assert x.shape[1] == 2, "Dim 1 must be 2, try transposing data, or do you have a 1d array in a 2d array?"
-        assert (x[:, 0] <= x[:, 1]).all(), "All left intervals must be less than right intervals"
+	if x.ndim == 2:
+		assert x.shape[1] == 2, "Dim 1 must be 2, try transposing data, or do you have a 1d array in a 2d array?"
+		assert (x[:, 0] <= x[:, 1]).all(), "All left intervals must be less than right intervals"
 
-    if c is not None:
-        c = np.array(c)
-        assert c.ndim == 1, "censoring array must be one dimensional"
-        assert c.shape[0] == x.shape[0], "censoring array must be same length as variable array"
+	if c is not None:
+		c = np.array(c)
+		assert c.ndim == 1, "censoring array must be one dimensional"
+		assert c.shape[0] == x.shape[0], "censoring array must be same length as variable array"
 
-        if x.ndim == 2:
-            assert not any(c[x[:, 0] == x[:, 1]] == 2), "Censor flag indicates interval censored but only has one failure time"
-            assert all(c[x[:, 0] != x[:, 1]] == 2), "Censor flag provided, but case where interval flagged as non interval censoring"
-            assert not any(
-                    (c !=  0) &
-                    (c !=  1) &
-                    (c != -1) &
-                    (c !=  2)
-                ), "Censoring value must only be one of -1, 0, 1, or 2"
-        else:
-            assert not any(
-                (c !=  0) &
-                (c !=  1) &
-                (c != -1)
-            ), "Censoring value must only be one of -1, 0, 1 for single dimension input"
-    else:
-        if x.ndim == 1:
-            c = np.zeros_like(x)
-        else:
-            c = np.zeros(x.shape[0])
-            c[x[:, 0] != x[:, 1]] = 2
+		if x.ndim == 2:
+			assert not any(c[x[:, 0] == x[:, 1]] == 2), "Censor flag indicates interval censored but only has one failure time"
+			assert all(c[x[:, 0] != x[:, 1]] == 2), "Censor flag provided, but case where interval flagged as non interval censoring"
+			assert not any(
+					(c !=  0) &
+					(c !=  1) &
+					(c != -1) &
+					(c !=  2)
+				), "Censoring value must only be one of -1, 0, 1, or 2"
+		else:
+			assert not any(
+				(c !=  0) &
+				(c !=  1) &
+				(c != -1)
+			), "Censoring value must only be one of -1, 0, 1 for single dimension input"
+	else:
+		c = np.zeros(x.shape[0])
+		if x.ndim != 1:
+			c[x[:, 0] != x[:, 1]] = 2
 
-    if n is not None:
-        n = np.array(n)
-        assert n.ndim == 1, "count array must be one dimensional"
-        assert n.shape[0] == x.shape[0], "count array must be same length as variable array."
-        assert (n > 0).all(), "count array can't be 0"
-    else:
-        # Do check here for groupby and binning
-        n = np.ones(x.shape[0])
+	if n is not None:
+		n = np.array(n)
+		assert n.ndim == 1, "count array must be one dimensional"
+		assert n.shape[0] == x.shape[0], "count array must be same length as variable array."
+		assert (n > 0).all(), "count array can't be 0"
+	else:
+		# Do check here for groupby and binning
+		n = np.ones(x.shape[0])
 
-    n = n.astype(np.int64)
-    c = c.astype(np.int64)
-    
-    x, c, n = group_xcn(x, c, n)
-    x, c, n = xcn_sort(x, c, n)
+	n = n.astype(np.int64)
+	c = c.astype(np.int64)
+	
+	x, c, n = group_xcn(x, c, n)
+	x, c, n = xcn_sort(x, c, n)
 
-    return x, c, n
+	return x, c, n
 
 def xcn_to_xrd(x, c=None, n=None):
 	"""
