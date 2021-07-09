5c1abf7f1bab78621e89a4b9cd2183dd8ab4c6e0
Derryn Knife <derrynknife@Derryns-MacBook-Air.local>
2021-07-10 07:16:46 +1000

    flake8 standard compliance work

--- surpyval/parametric/fitters/__init__.py
+++ surpyval/parametric/fitters/__init__.py
@@ -1,68 +1,81 @@
 import autograd.numpy as np
 
-# Functions Supporting the transformation of parameters for optimisation.
+
 def pass_through(x):
-	return x
+    return x
+
 
 def adj_relu(x):
-	return np.where(x >= 0, x + 1, np.exp(x))
+    return np.where(x >= 0, x + 1, np.exp(x))
+
 
 def inv_adj_relu(x):
-	return np.where(x >= 1, x - 1, np.log(x))
+    return np.where(x >= 1, x - 1, np.log(x))
+
 
 def rev_adj_relu(x):
-	return -np.where(x >= 0, x + 1, np.exp(x))
+    return -np.where(x >= 0, x + 1, np.exp(x))
+
 
 def inv_rev_adj_relu(x):
-	return np.where(x < -1, -x - 1, np.log(-x))
+    return np.where(x < -1, -x - 1, np.log(-x))
 
 
 def bounds_convert(x, bounds):
-	funcs = []
-	inv_f = []
-	for l, u in bounds:
-		if (l is None) and (u is None):
-			funcs.append(lambda x : pass_through(x))
-			inv_f.append(lambda x : pass_through(x))
-		elif (u is None):
-			funcs.append(lambda x : inv_adj_relu(x))
-			inv_f.append(lambda x : adj_relu(x))
-		elif (l is None):
-			if u != 0:
-				upper = np.min(x)
-			else:
-				upper = 0
-			funcs.append(lambda x : inv_rev_adj_relu(x - upper))
-			inv_f.append(lambda x : upper + rev_adj_relu(x))
+    funcs = []
+    inv_f = []
+    for lower, upper in bounds:
+        if (lower is None) and (upper is None):
+            funcs.append(lambda x: pass_through(x))
+            inv_f.append(lambda x: pass_through(x))
+        elif (upper is None):
+            funcs.append(lambda x: inv_adj_relu(x))
+            inv_f.append(lambda x: adj_relu(x))
+        elif (lower is None):
+            if upper != 0:
+                limit = np.min(x)
+            else:
+                limit = 0
+            funcs.append(lambda x: inv_rev_adj_relu(x - limit))
+            inv_f.append(lambda x: limit + rev_adj_relu(x))
 
-	transform = lambda params : np.array([f(p) for p, f in zip(params, funcs)])
-	inv_trans = lambda params : np.array([f(p) for p, f in zip(params, inv_f)])
+    def transform(params):
+        return np.array([f(p) for p, f in zip(params, funcs)])
 
-	return transform, inv_trans, funcs, inv_f
+    def inv_trans(params):
+        return np.array([f(p) for p, f in zip(params, inv_f)])
+
+    return transform, inv_trans, funcs, inv_f
 
 
 # Functions to support fixing parameters
 def fix_idx_and_function(dist, fixed, param_map, offset_index_inc, funcs):
-	if fixed is not None:
-		"""
-		Record to the model that parameters were fixed
-		"""
-		fixed_idx = [param_map[x] + offset_index_inc for x in fixed.keys()]
-		not_fixed = np.array([x for x in range(dist.k + offset_index_inc) if x not in fixed_idx])
+    if fixed is not None:
+        """
+        Record to the model that parameters were fixed
+        """
+        fixed_idx = [param_map[x] + offset_index_inc for x in fixed.keys()]
+        not_fixed = [x for x in
+                     range(dist.k + offset_index_inc) if x not in fixed_idx]
+        not_fixed = np.array(not_fixed)
 
-		def constraints(p):
-			params = [0] * (dist.k + offset_index_inc)
-			# params = np.empty(self.k + offset_index_inc)
-			for k, v in fixed.items():
-				params[param_map[k] + offset_index_inc] = funcs[param_map[k] + offset_index_inc](v)
-			for i, v in zip(not_fixed, p):
-				params[i] = v
-			return np.array(params)
+        def constraints(p):
+            params = [0] * (dist.k + offset_index_inc)
+            # params = np.empty(self.k + offset_index_inc)
+            for k, v in fixed.items():
+                params[param_map[k] + offset_index_inc] = (
+                    funcs[param_map[k] + offset_index_inc](v))
+            for i, v in zip(not_fixed, p):
+                params[i] = v
+            return np.array(params)
 
-		const = constraints
-	else:
-		const = lambda x : x
-		fixed_idx = []
-		not_fixed = np.array([x for x in range(dist.k + offset_index_inc)])
+        const = constraints
+    else:
 
-	return const, fixed_idx, not_fixed
+        def const(x):
+            return x
+
+        fixed_idx = []
+        not_fixed = np.array([x for x in range(dist.k + offset_index_inc)])
+
+    return const, fixed_idx, not_fixed
--- surpyval/parametric/fitters/mle.py
+++ surpyval/parametric/fitters/mle.py
@@ -1,113 +1,109 @@
 import autograd.numpy as np
 from autograd import jacobian, hessian
 from autograd.numpy.linalg import inv
-from autograd.numpy.linalg import pinv
 import copy
-
 from scipy.optimize import minimize
-from scipy.optimize import approx_fprime
-
-import surpyval
-import warnings
 import sys
 
-def _create_censor_flags(x_mle, gamma, c, dist):
-	if 2 in c:
-		l_flag = x_mle[:, 0] <= np.min([gamma, dist.support[0]])
-		r_flag = x_mle[:, 1] >= dist.support[1]
-		mask = np.vstack([l_flag, r_flag]).T
-		inf_c_flags = (mask).astype(int)
-		x_mle[(c == 2).reshape(-1, 1) & mask] = 1
-	else:
-		inf_c_flags = np.zeros_like(x_mle)
 
-	return inf_c_flags, x_mle
+def _create_censor_flags(x_mle, gamma, c, dist):
+    if 2 in c:
+        l_flag = x_mle[:, 0] <= np.min([gamma, dist.support[0]])
+        r_flag = x_mle[:, 1] >= dist.support[1]
+        mask = np.vstack([l_flag, r_flag]).T
+        inf_c_flags = (mask).astype(int)
+        x_mle[(c == 2).reshape(-1, 1) & mask] = 1
+    else:
+        inf_c_flags = np.zeros_like(x_mle)
+
+    return inf_c_flags, x_mle
+
 
 def mle(dist, x, c, n, t, const, trans, inv_fs, init, fixed_idx, offset):
-		"""
-		Maximum Likelihood Estimation (MLE)
+    """
+    Maximum Likelihood Estimation (MLE)
 
-		"""
-		if hasattr(dist, 'mle'):
-			return dist.mle(x, c, n, t, const, trans, inv_fs, init, fixed_idx, offset)
+    """
+    if hasattr(dist, 'mle'):
+        return dist.mle(x, c, n, t, const, trans,
+                        inv_fs, init, fixed_idx, offset)
 
-		old_err_state = np.seterr(invalid='raise', divide='raise', over='ignore', under='ignore')
+    old_err_state = np.seterr(invalid='raise',
+                              divide='raise',
+                              over='ignore',
+                              under='ignore')
 
-		results = {}
+    results = {}
 
-		# Need to flag entries where truncation is inf or -inf so that the autograd doesn't fail. Because
-		# autograd fails if it encounters any inf, nan, -inf etc even if they don't affect the gradient
-		# Very important to the autograd! And it worked, yay!
-		t_flags = np.ones_like(t)
-		t_mle = copy.copy(t)
-		# Create flags to indicate where the truncation values are infinite
-		t_flags[:, 0] = np.where(np.isfinite(t[:, 0]), 1, 0)
-		t_flags[:, 1] = np.where(np.isfinite(t[:, 1]), 1, 0)
-		# Convert the infinite values to a finite value to ensure the autodiff functions don't fail
-		t_mle[:, 0] = np.where(t_flags[:, 0] == 1, t[:, 0], 1)
-		t_mle[:, 1] = np.where(t_flags[:, 1] == 1, t[:, 1], 1)
+    # Need to flag entries where truncation is inf or -inf so that the autograd doesn't fail. Because
+    # autograd fails if it encounters any inf, nan, -inf etc even if they don't affect the gradient
+    # Very important to the autograd! And it worked, yay!
+    t_flags = np.ones_like(t)
+    t_mle = copy.copy(t)
+    # Create flags to indicate where the truncation values are infinite
+    t_flags[:, 0] = np.where(np.isfinite(t[:, 0]), 1, 0)
+    t_flags[:, 1] = np.where(np.isfinite(t[:, 1]), 1, 0)
+    # Convert the infinite values to a finite value to ensure the autodiff functions don't fail
+    t_mle[:, 0] = np.where(t_flags[:, 0] == 1, t[:, 0], 1)
+    t_mle[:, 1] = np.where(t_flags[:, 1] == 1, t[:, 1], 1)
 
-		results['t_flags'] = t_flags
-		results['t_mle'] = t_mle
+    results['t_flags'] = t_flags
+    results['t_mle'] = t_mle
 
-		# Create the objective function
-		def fun(params, offset=False, transform=True):
-			x_mle = np.copy(x)
-			if transform:
-				params = inv_fs(const(params))
-			if offset:
-				gamma = params[0]
-				params = params[1::]
-			else:
-				gamma = 0
+    # Create the objective function
+    def fun(params, offset=False, transform=True):
+        x_mle = np.copy(x)
+        if transform:
+            params = inv_fs(const(params))
+        if offset:
+            gamma = params[0]
+            params = params[1::]
+        else:
+            gamma = 0
 
-			inf_c_flags, x_mle = _create_censor_flags(x_mle, gamma, c, dist)
-			x_mle = x_mle - gamma
+        inf_c_flags, x_mle = _create_censor_flags(x_mle, gamma, c, dist)
+        x_mle = x_mle - gamma
 
-			return dist.neg_ll(x_mle, c, n, inf_c_flags, t_mle, t_flags, *params)
+        return dist.neg_ll(x_mle, c, n, inf_c_flags, t_mle, t_flags, *params)
 
-		jac  = jacobian(fun)
-		hess = hessian(fun)
+    jac  = jacobian(fun)
+    hess = hessian(fun)
 
-		try:
-			# First attempt to be with with jacobian and hessian from autograd
-			res  = minimize(fun, init, args=(offset, True), method='Newton-CG', jac=jac, hess=hess)
-			if not res.success:
-				raise Exception
-		except:
-			# If first attempt fails, try a second time with only jacobian from autograd
-			print("MLE with autodiff hessian and jacobian failed, trying without hessian", file=sys.stderr)
-			try:
-				res = minimize(fun, init, args=(offset, True), method='BFGS', jac=jac)
-				if not res.success:
-					raise Exception
-			except:
-				# If second attempt fails, try a third time with only jthe objective function
-				print("MLE with autodiff jacobian failed, trying without jacobian or hessian", file=sys.stderr)
-				try:
-					res = minimize(fun, init, args=(offset, True))
-				except:
-					# Something really went wrong
-					print("MLE FAILED: Likelihood function appears undefined; try alternate estimation method", file=sys.stderr)
-					np.seterr(**old_err_state)
-					return {}
+    try:
+        # First attempt to be with with jacobian and hessian from autograd
+        res  = minimize(fun, init, args=(offset, True), method='Newton-CG', jac=jac, hess=hess)
+        if not res.success:
+            raise Exception
+    except:
+        # If first attempt fails, try a second time with only jacobian from autograd
+        print("MLE with autodiff hessian and jacobian failed, trying without hessian", file=sys.stderr)
+        try:
+            res = minimize(fun, init, args=(offset, True), method='BFGS', jac=jac)
+            if not res.success:
+                raise Exception
+        except:
+            # If second attempt fails, try a third time with only jthe objective function
+            print("MLE with autodiff jacobian failed, trying without jacobian or hessian", file=sys.stderr)
+            try:
+                res = minimize(fun, init, args=(offset, True))
+            except:
+                # Something really went wrong
+                print("MLE FAILED: Likelihood function appears undefined; try alternate estimation method", file=sys.stderr)
+                np.seterr(**old_err_state)
+                return {}
 
-		p_hat = inv_fs(const(res.x))
-		results['_neg_ll'] = res.fun
-		results['hess_inv'] = inv(hessian(fun)(p_hat, *(offset, False)))
-		results['jac'] = jac
-		results['res'] = res
+    p_hat = inv_fs(const(res.x))
+    results['_neg_ll'] = res.fun
+    results['hess_inv'] = inv(hessian(fun)(p_hat, *(offset, False)))
+    results['jac'] = jac
+    results['res'] = res
 
-		if offset:
-			results['gamma'] = p_hat[0]
-			results['params'] = p_hat[1::]
-		else:
-			results['params'] = p_hat
+    if offset:
+        results['gamma'] = p_hat[0]
+        results['params'] = p_hat[1::]
+    else:
+        results['params'] = p_hat
 
-		np.seterr(**old_err_state)
+    np.seterr(**old_err_state)
 
-		return results
-
-
-
-
+    return results
