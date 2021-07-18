8fdfb61813a4bab2e5e48264cbf61e46f1adf932
Derryn Knife <derrynknife@Derryns-MacBook-Air.local>
2021-07-18 20:42:19 +1000

    version 0.6.1 actual

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="surpyval",
-    version="0.6.0",
+    version="0.6.1",
     author="Derryn Knife",
     author_email="derryn@reliafy.com",
     description="A python package for survival analysis",
