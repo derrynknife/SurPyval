801dee003877b7274870931f91d3c01841cb34db
derryn-knife <derryn.knife@gmail.com>
2022-06-19 17:11:47 +0800

    moved counting to renewal

--- surpyval/counting/__init__.py
+++
@@ -1,5 +0,0 @@
-from .nhpp import Duane, CoxLewis, Crow, CrowAMSAA, HPP
-from .grp import GRP
-
-from .nonparametric import NonParametricCounting
-
---
+++ surpyval/renewal/__init__.py
@@ -0,0 +1,5 @@
+from .parametric.nhpp import Duane, CoxLewis, Crow, CrowAMSAA, HPP
+from .parametric.grp import GRP
+
+from .nonparametric import NonParametricCounting
+
