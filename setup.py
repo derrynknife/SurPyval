d0736f65986825a79c85bd9be3552c06c9c890e7
derrynknife <derryn.knife@gmail.com>
2022-09-11 19:39:16 +1000

    version 0.10.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="surpyval",
-    version="0.10.4",
+    version="0.10.5",
     author="Derryn Knife",
     author_email="derryn@reliafy.com",
     description="A python package for survival analysis",
