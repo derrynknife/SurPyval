e93404109326a9c517cdf4a6d086da7edec3f6ff
derrynknife <derryn.knife@gmail.com>
2022-09-24 21:10:39 +1000

    added series and parallel models to parametric module

--- surpyval/parametric/__init__.py
+++ surpyval/parametric/__init__.py
@@ -28,4 +28,5 @@ from .logistic import Logistic
 from .loglogistic import LogLogistic
 from .mixture_model import MixtureModel
 from .dist_from_hazard import Distribution
-
+from .series import SeriesModel
+from .parallel import ParallelModel
