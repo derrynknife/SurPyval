2ebc4e6a02707452df500bb97cc278dd88d65175
derrynknife <derryn.knife@gmail.com>
2023-01-11 19:01:09 +1000

    current_age added to renewal frunction

--- surpyval/renewal/renewal.py
+++ surpyval/renewal/renewal.py
@@ -7,19 +7,25 @@ class Renewal:
     def __init__(self, model):
         self.model = model
 
-    def simulate_over_interval(self, T, N=100_000):
+    def simulate_over_interval(self, T, N=100_000, current_age=None):
         running = 0
         x = self.model.random(2 * N).tolist()
         aggregate_timeline = defaultdict(lambda: 0)
         # Put in values for times 0 and T
         # this ensures there is an entry at 0 and T
-        aggregate_timeline[0] = 0
+        if current_age is None:
+            aggregate_timeline[0] = 0
+        else:
+            aggregate_timeline[current_age] = 0
         aggregate_timeline[T] = 0
         counted = 0
 
         while counted < N:
             try:
-                xi = x.pop()
+                if running == 0 and current_age is not None:
+                    xi = self.model.random(1, a=current_age).item()
+                else:
+                    xi = x.pop()
             except Exception:
                 # For now, assume 2 events in the window
                 x = self.model.random(2 * N).tolist()
