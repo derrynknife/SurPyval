054c8d735096ad8756e51260796df776d67740c3
derrynknife <derryn.knife@gmail.com>
2023-05-14 20:13:42 +1000

    updated log rank split to account for corner cases

--- surpyval/regression/forest/log_rank_split.py
+++ surpyval/regression/forest/log_rank_split.py
@@ -4,11 +4,20 @@ from typing import Iterable
 import numpy as np
 from numpy.typing import NDArray
 
-# from sksurv.compare import compare_survival
+from surpyval.utils import xcnt_to_xrd
 
-# from surpyval.utils.surv_sksurv_transformations import (  # sksurv's log-rank
-#     surv_xZc_to_sksurv_Xy,
-# )
+
+# Inner function used in ensuring the min_leaf_failures constraint is
+# respected
+def breaks_min_leaf_failures_constraint(Z, u, v, c, min_leaf_failures):
+    left_child_samples = len(*np.where(np.logical_and(Z[:, u] <= v, c == 0)))
+    right_child_samples = len(np.where(np.logical_and(Z[:, u] > v, c == 0))[0])
+    if (
+        left_child_samples < min_leaf_failures
+        or right_child_samples < min_leaf_failures
+    ):
+        return True
+    return False
 
 
 def log_rank_split(
@@ -69,60 +78,22 @@ def log_rank_split(
         min_leaf_failures constraint.
     """
 
-    # Sort x, Z, and c, in x, making sure Z is 2d (required for future calcs)
-    sort_idxs = np.argsort(x)
-    x = x[sort_idxs]
-    c = c[sort_idxs]
-    if Z.ndim == 1:
-        Z = np.reshape(Z[sort_idxs], (1, -1)).transpose()
-    else:
-        Z[sort_idxs]
-
-    # Calculate the d vector, where d[j] is the number of distinct deaths
-    # at t[j]
-    death_indices = np.where(c == 0)[0]  # Uncensored => deaths
-    death_xs = x[death_indices]  # Death times
-
-    # The 't' and 'd' vectors, that is t[j] is the j-th dinstinct (uncensored)
-    # death time, and d[j] is the number of distinct deaths at that time
-    t, d = np.unique(death_xs, return_counts=True)
-    m = len(t)  # How many unique times in the samples there are
-
-    # Now the Y vector needs to be calculated
-    # Y[j] = the number of people still 'at risk' at time t[j], that is the
-    # sum of samples who's survival times are greater than t[j] (irrelevant
-    # of censorship)
-    Y = np.array([len(x[x >= t_j]) for t_j in t])
-
     # Now let's find the best (u, v) pair
     max_log_rank_magnitude = float("-inf")
     best_u = -1  # Placeholder value
     best_v = -float("inf")  # Placeholder value
 
-    # Inner function used in ensuring the min_leaf_failures constraint is
-    # respected
-    def breaks_min_leaf_failures_constraint():
-        left_child_samples = len(
-            np.where(np.logical_and(Z[:, u] <= v, c == 0))[0]
-        )
-        right_child_samples = len(
-            np.where(np.logical_and(Z[:, u] > v, c == 0))[0]
-        )
-        if (
-            left_child_samples < min_leaf_failures
-            or right_child_samples < min_leaf_failures
-        ):
-            return True
-        return False
-
     for u in feature_indices_in:
-        for v in np.unique(Z[:, u])[:-1]:
+        Z_u = Z[:, u]
+        for v in np.unique(Z_u):
             # Discard the (u, v) pair if it means a leaf will
             # have < min_leaf_failures samples
-            if breaks_min_leaf_failures_constraint():
+            if Z_u[Z_u <= v].size < min_leaf_failures:
+                continue
+            elif Z_u[Z_u > v].size < min_leaf_failures:
                 continue
 
-            abs_log_rank = abs(log_rank(u, v, x, Z, c, t, d, Y, m))
+            abs_log_rank = log_rank(u, v, x, Z, c)
 
             if abs_log_rank > max_log_rank_magnitude:
                 max_log_rank_magnitude = abs_log_rank
@@ -138,99 +109,49 @@ def log_rank(
     x: NDArray,
     Z: NDArray,
     c: NDArray,
-    t: NDArray,
-    d: NDArray,
-    Y: NDArray,
-    m: int,
 ) -> float:
     """Returns L(u, v)."""
-
-    # Define the d_L and Y_L vectors
-    d_L = np.zeros(m)
-    Y_L = np.zeros(m)
 
     # Get sample-indices (i) of those that would end up in the left child
     left_child_indices = np.where(Z[:, u] <= v)[0]
     left_child_x = x[left_child_indices]
     left_child_c = c[left_child_indices]
 
-    for j in range(m):
-        # Number of uncensored deaths at t[j]
-        d_L[j] = np.sum(left_child_x[left_child_c == 0] == t[j])
+    left_child_x, idx = np.unique(left_child_x, return_inverse=True)
+    d_L = np.bincount(idx, weights=1 - left_child_c)
+    do_L = np.bincount(idx, weights=left_child_c)
+    all_x, Y, d = xcnt_to_xrd(x, c)
 
-        # Number 'at risk', that is those still alive + have an event (death
-        # or censor) at t[j]
-        Y_L[j] = np.sum(left_child_x >= t[j])
+    # expand d_L to match all_x
+    expanded_d_L = np.zeros_like(all_x)
+    expanded_do_L = np.zeros_like(all_x)
+    x_l_indices = np.in1d(all_x, left_child_x).nonzero()[0]
+    expanded_d_L[x_l_indices] = d_L
+    expanded_do_L[x_l_indices] = do_L
+    d_L = expanded_d_L
+    do_L = expanded_do_L
 
-    # Perform the j-sums
-    numerator = 0
-    denominator_inside_sqrt = 0  # Must sqrt() after j loop
+    # Find the risk set from the expanded d_L and do_L
+    # These are the event and censored counts at each x
+    Y_L = (d_L.sum() + do_L.sum()) - d_L.cumsum() + d_L - do_L.cumsum() + do_L
 
-    for j in range(m):
-        if not Y[j] >= 2:
-            # Denominator contribution would be undefined
-            # (Y[j] - 1 <= 0 -> bad!)
-            continue
-        numerator += d_L[j] - Y_L[j] * d[j] / Y[j]
-        denominator_inside_sqrt += (
-            Y_L[j]
-            / Y[j]
-            * (1 - Y_L[j] / Y[j])
-            * (Y[j] - d[j])
-            / (Y[j] - 1)
-            * d[j]
-        )
+    # Filter to where Y > 1
+    mask = Y > 1
+    Y_L = Y_L[mask]
+    Y = Y[mask]
+    d_L = d_L[mask]
+    d = d[mask]
 
-    L_u_v_return = numerator / sqrt(denominator_inside_sqrt)
+    numerator = np.sum(d_L - Y_L * (d / Y))
+    denominator_inside_sqrt = np.sum(
+        (Y_L / Y) * (1.0 - Y_L / Y) * (Y - d) / (Y - 1) * d
+    )
 
-    return L_u_v_return
+    if denominator_inside_sqrt == 0:
+        return -float("inf")
 
-    # # Transform to scikit-survival form
-    # if Z.ndim == 1:
-    #     Z = np.reshape(Z, (1, -1)).transpose()
-    # X, y = surv_xZc_to_sksurv_Xy(x=x, Z=Z, c=c)
-
-    # # Best values
-    # best_feature_index = -1
-    # best_feature_value = float("-inf")
-    # best_log_rank = float("-inf")
-
-    # # Inner function used in ensuring the min_leaf_failures constraint is
-    # # respected
-    # def breaks_min_leaf_failures_constraint():
-    #     left_child_samples = len(
-    #         np.where(np.logical_and(Z[:, u] <= v, c == 0))[0]
-    #     )
-    #     right_child_samples = len(
-    #         np.where(np.logical_and(Z[:, u] > v, c == 0))[0]
-    #     )
-    #     if (
-    #         left_child_samples < min_leaf_failures
-    #         or right_child_samples < min_leaf_failures
-    #     ):
-    #         return True
-    #     return False
-
-    # # Loop over features
-    # for u in range(X.shape[1]):
-    #     possible_feature_values = np.unique(X[:, u])
-
-    #     #If there's <2 unique values to consider, move on to the next feature
-    #     if len(possible_feature_values) < 2:
-    #         continue
-
-    #     # Else, go over each possible feature value
-    #     for i, v in enumerate(possible_feature_values):
-    #         if breaks_min_leaf_failures_constraint():
-    #             continue
-
-    #         split = (v + possible_feature_values[i + 1]) * 0.5
-
-    #         groups = (X[:, u] <= split).astype(int)
-    #         log_rank_u_v, _ = compare_survival(y, groups)
-    #         if log_rank_u_v > best_log_rank:
-    #             best_feature_index = u
-    #             best_feature_value = split
-    #             best_log_rank = log_rank_u_v
-
-    # return best_feature_index, best_feature_value
+    try:
+        v = np.abs(numerator / sqrt(denominator_inside_sqrt))
+        return v
+    except ZeroDivisionError:
+        raise ValueError("Numerator or denominator is NaN")
